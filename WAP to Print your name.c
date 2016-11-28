a030963eab5fd1e63fa5257c2288f2e362cb630c
rupak1998 <rupak.adhikari1998@gmail.com>
2016-11-28 12:54:12 +0545

    Add files via upload

---
+++ WAP to Print your name.c
@@ -0,0 +1,18 @@
+/*
+Student Name : Rupak Adhikari
+Subject : Programme Fundamentals
+Roll No :
+LabSheet No : O1
+Program : WAP to print your name
+Date : 28TH NOV
+*/
+
+#include<stdio.h>
+#include<conio.h>
+
+int main() {
+printf("rupak");
+
+getch();
+return(0):
+}
---
+++ calculate its square and square root.c
@@ -0,0 +1,35 @@
+/*
+Student Name : Rupak Adhikari
+Subject : Progamming Fundamentals
+Lab Sheet No : 07
+Program : Calculate Its Square
+*/
+
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+// math.h should be include if  math function are used
+int main(){
+
+int a,sqr,sqrroot;
+
+printf(" Enter Your Value\n ");
+scanf("%d",&a);
+
+sqr=a*a;
+sqrroot=sqrt(a);
+// sqr denote for squre of value
+// sqrroot denote for  square root of value
+
+printf(" The square of your value is: %d\n",sqr);
+printf(" The square root of your value is : %d\n",sqrroot);
+
+getch();
+return(0);
+}
+
+
+
+
+
+}
